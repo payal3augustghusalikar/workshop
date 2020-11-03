ef67f8469aa01722663369ac4c42afe196a71dde
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-11-03 16:07:08 +0530

    Added : empty tic_tac_toe board

--- Tic_Tac_toe.sh
+++ Tic_Tac_toe.sh
@@ -1,3 +1,26 @@
 #! /bin/bash -x 
 echo "welcome to tic tac toe game"
+function resetTheBoard()
+{
 
+ for (( i=1; i<=9; i++ ))
+ do
+   boardOfTicTacToe["$i"]="-";
+
+ done
+ displayBoard
+}
+
+
+function displayBoard()
+{
+   echo "    |---|---|---|"
+   echo "    | "${boardOfTicTacToe[1]}" | "${boardOfTicTacToe[2]}" | "${boardOfTicTacToe[3]}" |"
+   echo "    |---|---|---|"
+   echo "    | "${boardOfTicTacToe[4]}" | "${boardOfTicTacToe[5]}" | "${boardOfTicTacToe[6]}" |"
+   echo "    |---|---|---|"
+   echo "    | "${boardOfTicTacToe[7]}" | "${boardOfTicTacToe[8]}" | "${boardOfTicTacToe[9]}" |"
+   echo "    |---|---|---|"
+
+}
+resetTheBoard
